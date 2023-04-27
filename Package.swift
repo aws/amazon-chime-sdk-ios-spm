62b39da33452399f80177b012a20dc65cf4389fd
Dingyi Shen <88684830+dingyishen-amazon@users.noreply.github.com>
2023-04-27 14:44:04 -0700

    Support for Swift  Package Manager added (#1)
    * Support for Swift Package Manager added

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+// Current stable version of the Amazon Chime SDK for iOS
+let SDKVersion = "0.23.0"
+let SDKMediaVersion = "0.18.0"
+let SDKMachineLearningVersion = "0.2.0"
+
+// TODO: Update url during the first release
+// Hosting url where the release artifacts are hosted.
+let hostingUrl = "<TBD>"
+
+// TODO: Update checksums during the first release
+// Checksums for SDKs to verify that the hosted archive file matches the archive declared in the manifest file.
+let SDKChecksum = "SHA256_CHECKSUM_PLACEHOLDER"
+let SDKMediaChecksum = "SHA256_CHECKSUM_PLACEHOLDER"
+let SDKMachineLearningChecksum = "SHA256_CHECKSUM_PLACEHOLDER"
+
+let package = Package(
+    name: "AmazonChimeSDK",
+    products: [
+        .library(name: "AmazonChimeSDK", targets: ["AmazonChimeSDK", "AmazonChimeSDKMedia"]),
+        .library(name: "AmazonChimeSDKMedia", targets: ["AmazonChimeSDKMedia"]),
+        .library(name: "AmazonChimeSDKMachineLearning", targets: ["AmazonChimeSDKMachineLearning"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AmazonChimeSDK",
+            url: "\(hostingUrl)/AmazonChimeSDK-\(SDKVersion).zip",
+            checksum: SDKChecksum
+        ),
+        .binaryTarget(
+            name: "AmazonChimeSDKMedia",
+            url: "\(hostingUrl)/AmazonChimeSDKMedia-\(SDKMediaVersion).zip",
+            checksum: SDKMediaChecksum
+        ),
+        .binaryTarget(
+            name: "AmazonChimeSDKMachineLearning",
+            url: "\(hostingUrl)/AmazonChimeSDKMachineLearning-\(SDKMachineLearningVersion).zip",
+            checksum: SDKMachineLearningChecksum
+        )
+    ]
+)
