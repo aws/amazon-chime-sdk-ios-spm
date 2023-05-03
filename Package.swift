2066b425ac12a3d6e8ef09130da3aa9b5eab184b
Dingyi Shen <dingyshe@amazon.com>
2023-05-03 00:26:02 -0700

    Update hosting url

--- Package.swift
+++ Package.swift
@@ -6,9 +6,8 @@ let SDKVersion = "0.23.0"
 let SDKMediaVersion = "0.18.0"
 let SDKMachineLearningVersion = "0.2.0"
 
-// TODO: Update url during the first release
 // Hosting url where the release artifacts are hosted.
-let hostingUrl = "<TBD>"
+let hostingUrl = "https://amazon-chime-sdk-ios.s3.amazonaws.com"
 
 // TODO: Update checksums during the first release
 // Checksums for SDKs to verify that the hosted archive file matches the archive declared in the manifest file.
@@ -26,17 +25,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonChimeSDK",
-            url: "\(hostingUrl)/AmazonChimeSDK-\(SDKVersion).zip",
+            url: "\(hostingUrl)/sdk-without-bitcode/\(SDKVersion)/spm/AmazonChimeSDK-\(SDKVersion).zip",
             checksum: SDKChecksum
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMedia",
-            url: "\(hostingUrl)/AmazonChimeSDKMedia-\(SDKMediaVersion).zip",
+            url: "\(hostingUrl)/media-without-bitcode/\(SDKMediaVersion)/spm/AmazonChimeSDKMedia-\(SDKMediaVersion).zip",
             checksum: SDKMediaChecksum
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMachineLearning",
-            url: "\(hostingUrl)/AmazonChimeSDKMachineLearning-\(SDKMachineLearningVersion).zip",
+            url: "\(hostingUrl)/machine-learning-without-bitcode/\(SDKMachineLearningVersion)/spm/AmazonChimeSDKMachineLearning-\(SDKMachineLearningVersion).zip",
             checksum: SDKMachineLearningChecksum
         )
     ]
