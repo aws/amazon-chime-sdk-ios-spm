b056107dea4c232c73a007bfbecffa2d154501fe
George Zhang <georgezy@amazon.com>
2024-04-01 16:42:25 -0700

    Update ML checksum

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let hostingUrl = "https://amazon-chime-sdk-ios.s3.amazonaws.com"
 // Checksums for SDKs to verify that the hosted archive file matches the archive declared in the manifest file.
 let SDKChecksum = "06635de26c5fcb48841e489774a550db4f87e4bd76077b919b24fd735f82fa6c"
 let SDKMediaChecksum = "85f0b56c01a99d87e6f8f9198a9016f1adde93867fa53234da4db0e4efcca36d"
-let SDKMachineLearningChecksum = "9f38aef436f9dcdcf2687ed35b6c72cbbd495d3ae1ef1f97d4f53bd0c2f974e1"
+let SDKMachineLearningChecksum = "922fd02531b67971dacbb57353281eed94e22df475e9558d1a98d5c864a9af88"
 
 let package = Package(
     name: "AmazonChimeSDK",
     products: [
         .library(name: "AmazonChimeSDK", targets: ["AmazonChimeSDK", "AmazonChimeSDKMedia"]),
-        .library(name: "AmazonChimeSDKMedia", targets: ["AmazonChimeSDKMedia"]),
-        .executable(name: "AmazonChimeSDKMachineLearning",  targets: ["AmazonChimeSDKMachineLearning"])
+        .library(name: "AmazonChimeSDKMedia", type: .dynamic, targets: ["AmazonChimeSDKMedia"]),
+        .library(name: "AmazonChimeSDKMachineLearning", targets: ["AmazonChimeSDKMachineLearning"])
     ],
     targets: [
         .binaryTarget(
@@ -34,7 +34,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMachineLearning",
-            url: "\(hostingUrl)/machine-learning/\(SDKMachineLearningVersion)/spm/AmazonChimeSDKMachineLearning-\(SDKMachineLearningVersion).zip",
+            url: "\(hostingUrl)/machine-learning/\(SDKMachineLearningVersion)/spm/AmazonChimeSDKMachineLearning-0.3.0.1.zip",
             checksum: SDKMachineLearningChecksum
         )
     ]
