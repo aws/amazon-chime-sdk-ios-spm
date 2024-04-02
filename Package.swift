e431ff39ccbc5b7b818d6d3f2c31419073def663
George Zhang <georgezy@amazon.com>
2024-04-01 17:29:22 -0700

    Update ML SDK url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let hostingUrl = "https://amazon-chime-sdk-ios.s3.amazonaws.com"
 // Checksums for SDKs to verify that the hosted archive file matches the archive declared in the manifest file.
 let SDKChecksum = "06635de26c5fcb48841e489774a550db4f87e4bd76077b919b24fd735f82fa6c"
 let SDKMediaChecksum = "85f0b56c01a99d87e6f8f9198a9016f1adde93867fa53234da4db0e4efcca36d"
-let SDKMachineLearningChecksum = "2c6b955c2278d3389ea05314a24608c18c280858a523395d2b295a82f18bc97c"
+let SDKMachineLearningChecksum = "d926bdf203ea62d743cac508f206d7dc93e228e537d23193a93f1599e148b58b"
 
 let package = Package(
     name: "AmazonChimeSDK",
@@ -34,7 +34,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMachineLearning",
-            url: "\(hostingUrl)/machine-learning/\(SDKMachineLearningVersion)/spm/AmazonChimeSDKMachineLearning-0.3.0.2.zip",
+            url: "\(hostingUrl)/machine-learning/\(SDKMachineLearningVersion)/spm/AmazonChimeSDKMachineLearning-0.3.0.3.zip",
             checksum: SDKMachineLearningChecksum
         )
     ]
