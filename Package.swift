6d5a29dc414ab3bf079f026288a12109eef017bd
dylonChime <ddylon@amazon.com>
2025-04-01 15:28:32 -0700

    Bump SDKVersion to 0.27.1

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // Current stable version of the Amazon Chime SDK for iOS
-let SDKVersion = "0.27.0"
+let SDKVersion = "0.27.1"
 let SDKMediaVersion = "0.24.0"
 let SDKMachineLearningVersion = "0.3.1"
 
@@ -10,7 +10,7 @@ let SDKMachineLearningVersion = "0.3.1"
 let hostingUrl = "https://amazon-chime-sdk-ios.s3.amazonaws.com"
 
 // Checksums for SDKs to verify that the hosted archive file matches the archive declared in the manifest file.
-let SDKChecksum = "7b1c1d84cb6e1e61000936c9636a36eec25e6d2b03f9bbaace1e5dae5425afb8"
+let SDKChecksum = "d49cdd47cdadad79f2418927f1e08935b2f03e7de1b2021698a5441f0bd53e84"
 let SDKMediaChecksum = "ff4cfce4adbf3315fb214e1f2da0f53ddd9dc25dcf08a78826e8fcc088718d2a"
 let SDKMachineLearningChecksum = "d0968e47808f5c1a8b83b7383d9a76435eecf018d3b7aa2aa8a65205d212f31c"
 
